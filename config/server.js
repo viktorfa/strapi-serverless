46212610c3e5c7c85d521347daab577233206b14
Viktor Andersen <vikfand@gmail.com>
2020-08-17 00:20:29 +0200

    Bootstrap Strapi app with quickstart.

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c3f7ff3fe441450e2ad98a7314850040'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8fecadbe-4710-4485-a75a-2d8c420cd13b'
+};
